f325316471fe9f8cd7eed4048c834cfcbd9c1556
jacquelynneheiman1209 <jacquelynneheiman95@gmail.com>
2024-04-15 13:34:36 -0700

    Restarted the project so I could document how to do it in case I wanted to create a Youtube video series on it

---
+++ PicrossGame/Assets/Scripts/LevelData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Level Data", menuName = "Picross/New Level Data", order = 50)]
+public class LevelData : ScriptableObject
+{
+    public Texture2D sourceImage;
+}
